ebb8a8b04888c3cd2bb6b38e8b8ce76150e1547e
Harsh Singhvi <root@harshsinghvi.com>
2023-11-06 02:25:27 +0530

    MILESTONE: GoLang Basic TODO API no database

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # golang-postgres-kubernetes
+
+## practice
+
+- postgressql - indexing, explain querry
+- GoLang - APIs, concurrency
+- Kubernetes EKS - Autoscaling, load balencing,
+- API LoadTesting - Apache Benchmark
+
+## resources
+
+- Linkedin Learning (kubernetes, golang courses)
+- udemy hnsr course db
+- https://go.dev/doc/tutorial/web-service-gin
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	// "fmt"
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type Todo struct {
+    ID     string  `json:"id"`
+    Text  string  `json:"text"`
+    Completed bool  `json:"completed"`
+	// Date  float64 `json:"date"` // TODO: implement latter
+}
+var TODOS = []Todo{
+    {ID: "1", Text: "Task 1", Completed: false},
+    {ID: "2", Text: "Task 2", Completed: false},
+    {ID: "3", Text: "Task 3", Completed: false},
+}
+
+func getTodos(c *gin.Context) {
+	id := c.Query("id")
+	// completed := c.Query("completed") == "true" // TODO: implement this filter
+
+	if id == "" {
+		c.IndentedJSON(http.StatusOK, TODOS)
+		return
+	}
+	
+	for _, a := range TODOS {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+
+	c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "not found"})
+
+}
+
+func postTodos(c *gin.Context) {
+    var newTodo Todo
+
+    // Call BindJSON to bind the received JSON to
+    if err := c.BindJSON(&newTodo); err != nil {
+        return
+    }
+
+	if newTodo.Text == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "no text"})
+		return
+	}
+
+	for _, a := range TODOS {
+		if(a.ID == newTodo.ID){
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Duplicate ID"})
+			return
+		}
+	}
+    // Add the new album to the slice.
+    TODOS = append(TODOS, newTodo)
+    c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func updateTodos(c *gin.Context){
+	id := c.Param("id")
+	var updateTodo Todo
+
+    // Call BindJSON to bind the received JSON to
+    if err := c.BindJSON(&updateTodo); err != nil {
+        return
+    }
+	
+	if updateTodo.ID == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "no ID"})
+		return
+	}
+
+	for index, a := range TODOS {
+        if a.ID == id {
+			if updateTodo.Text != "" {
+				TODOS[index].Text = updateTodo.Text
+			}
+			TODOS[index].Completed = updateTodo.Completed
+            c.IndentedJSON(http.StatusOK, TODOS[index])
+            return
+        }
+    }
+
+	c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "invalid ID"})
+}
+
+func deleteTodos(c *gin.Context){
+	id := c.Param("id")
+
+	for index, a := range TODOS {
+		if a.ID == id{
+			TODOS = append(TODOS[:index], TODOS[index+1:]...)
+			c.IndentedJSON(http.StatusOK, TODOS)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "no ID"})
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/todos", getTodos)
+	router.POST("/todos", postTodos)
+	router.PUT("/todos/:id", updateTodos)
+	router.DELETE("/todos/:id", deleteTodos)
+
+    router.Run("localhost:8080")
+}
